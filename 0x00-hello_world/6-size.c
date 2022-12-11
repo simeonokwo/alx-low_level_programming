e673dc4b0cc4e72a74ae72b40d6cb036c5c2c975
simeonokwo <simeonokwo@gmail.com>
2022-12-11 07:59:41 -0800

    sizeoff

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,23 +1,20 @@
 #include <stdio.h>
-#include <unistd.h>
 /**
  * main - Entry point
- * Description: prints alphabet in lowercase then uppercase
- * Return: Always 0 (success)
+ *
+ *Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	int ch;
-
-	for (ch = 'a'; ch <= 'z'; ch++)
-	{
-		putchar(ch);
-	}
-	for (ch = 'A'; ch <= 'Z'; ch++)
-	{
-		putchar(ch);
-	}
-	putchar('\n');
-	return (0);
+	char charType;
+	int intType;
+	long int longIntType;
+	long long longLongType;
+	float floatType;
+		printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+		printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+		printf("Size of a long int: %zu byte(s)\n", sizeof(longIntType));
+		printf("Size of a long long int: %zu byte(s)\n", sizeof(longLongType));
+		printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
+		return (0);
 }
