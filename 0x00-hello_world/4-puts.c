52961d811b4af79338662503e15dc8697993f02d
simeonokwo <simeonokwo@gmail.com>
2022-12-11 06:26:03 -0800

    hello hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-/*
+/**
  * main - Entry
- * Return: Always 0
- * */
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
