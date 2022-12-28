898387a0826c11cd24f366805547b329d3eadaea
simeonokwo <simeonokwo@gmail.com>
2022-12-28 08:32:44 -0800

    alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,24 @@
 #include "main.h"
 
-
 /**
-* main - check the code
-*
-* Return: Always 0.
-*/
-	int main(void)
+ * print_alphabet_x10 - print alphabets
+ * Return: always 0 (success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int a;
+	int b;
+
+	a = 0;
+
+	while (a < 10)
 	{
-		print_alphabet();
-		return (0);
+		for (b = 'a'; b <= 'z'; b++)
+		{
+			_putchar(b);
+		}
+		_putchar('\n');
+		a++;
 	}
+}
